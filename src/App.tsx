22d6a2f43f87b9eaf627d5f0b06aba690e9f4f87
Ujjwal Saini <98773763+Acid-star@users.noreply.github.com>
2025-05-11 00:29:45 +0530

    Create missing main.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>CodeAcademy</h1>
+      <p>Learn & Test Your Coding Skills</p>
+    </div>
+  )
+}
+
+export default App
